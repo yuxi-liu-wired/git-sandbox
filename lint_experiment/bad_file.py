876d13e86d9ea81416f93694110024ae67c24cd2
Yuxi Liu <yuxi.liu.1995@gmail.com>
2024-01-09 20:26:52 -0800

    lint test

---
+++ lint_experiment/bad_file.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return 1 + fibonacci(n-1) + fibonacci(n-2)
+    
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
